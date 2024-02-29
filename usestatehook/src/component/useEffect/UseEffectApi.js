8ae456970e6f8d9d178065a514d9ee3949beafe7
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-02-29 14:18:52 +0530

    use effect

--- usestatehook/src/component/useEffect/UseEffectApi.js
+++ usestatehook/src/component/useEffect/UseEffectApi.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react'
+import Loading from './Loading';
 
 const UseEffectApi = () => {
 
     const [users,setUsers]=useState([]);
+    const [loading, setLoading]=useState(true);
 
    const getUsers=async()=>{
+
+    try{
+        setLoading(false)
      const res = await fetch('https://jsonplaceholder.typicode.com/posts')
     //   const data= await res.json();
     //   console.log(data);
        setUsers(await res.json());
+
+    }catch(error){
+        console.log("my error is" + error)
+    }
     }
 
 
@@ -16,8 +25,9 @@ const UseEffectApi = () => {
       getUsers();
    },[])
 
-
-
+   if(loading){
+     return <Loading/>
+   }
 
   return (
     <>
